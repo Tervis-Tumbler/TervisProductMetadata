06cfcb8ae839ead6b5304a21103bd936df02d6b1
Chris Magnuson <hourglasssand@gmail.com>
2019-08-06 15:16:32 -0400

    Move function from other module

--- TervisProductMetadata.js
+++ TervisProductMetadata.js
@@ -1,4 +1,8 @@
-import { Add_MemberScriptProperty } from '@tervis/tervisutilityjs'
+import {
+    Add_MemberScriptProperty,
+    New_HashTableIndex,
+    ConvertFrom_StringUsingRegexCaptureGroup
+} from '@tervis/tervisutilityjs'
 
 const $IsBrowser = !(typeof window === 'undefined');
 
@@ -109,4 +113,84 @@ export async function Get_TervisProductColorCodeToMarketingNameMapping() {
 
     return await fetch(`https://unpkg.com/@tervis/tervisproductmetadata@${versionOfColorCodeToMarketingNameMapping}/ColorCodeToMarketingNameMapping.json`)
     .then(response => response.json())
+}
+
+export async function Get_TervisProductPrintImageTemplateSizeAndFormType ({
+    $PrintImageTemplateName
+}) {
+    var $PrintImageTemplateNameToSizeAndFormTypeIndex = await Get_TervisProductPrintImageTemplateNameToSizeAndFormTypeIndex()
+    let {Size, FormType} = $PrintImageTemplateNameToSizeAndFormTypeIndex[$PrintImageTemplateName]
+    
+    if (Array.isArray(FormType)) {
+        var $FormType = FormType[0]
+    } else {
+        var $FormType = FormType
+    }
+    
+    return {$Size: Size, $FormType}
+}
+
+var $SizeAndFormTypeToImageTemplateNamesIndex
+async function Get_SizeAndFormTypeToImageTemplateNamesIndex () {
+    if (!$SizeAndFormTypeToImageTemplateNamesIndex) {
+        var $ProductMetadata = await Get_TervisProductMetaData()
+        $SizeAndFormTypeToImageTemplateNamesIndex = New_HashTableIndex({$InputObject: $ProductMetadata, $PropertyToIndex: "SizeAndFormTypes"})
+    }
+    
+    return $SizeAndFormTypeToImageTemplateNamesIndex
+}
+
+export async function Get_TervisProductImageTemplateName ({
+    $Size,
+    $FormType,
+    $TemplateType,
+    $PrintTemplateType
+}) {
+    var $SizeAndFormTypeToImageTemplateNamesIndex = await Get_SizeAndFormTypeToImageTemplateNamesIndex()
+    let $Template = $SizeAndFormTypeToImageTemplateNamesIndex[`${$Size}${$FormType}`].ImageTemplateName[$TemplateType]
+    if (!$PrintTemplateType) {
+        return $Template
+    } else {
+        return $Template[$PrintTemplateType]
+    }
+}
+
+
+export async function Get_TervisProductMetaDataUsingIndex ({
+    $Size,
+    $FormType
+}) {
+    var $SizeAndFormTypeToImageTemplateNamesIndex = await Get_SizeAndFormTypeToImageTemplateNamesIndex()
+    return $SizeAndFormTypeToImageTemplateNamesIndex[`${$Size}${$FormType}`]
+}
+
+var $PrintImageTemplateNameToSizeAndFormTypeIndex
+async function Get_TervisProductPrintImageTemplateNameToSizeAndFormTypeIndex () {
+    if (!$PrintImageTemplateNameToSizeAndFormTypeIndex) {
+        var $ProductMetadata = await Get_TervisProductMetaData()
+        $PrintImageTemplateNameToSizeAndFormTypeIndex = New_HashTableIndex({ $InputObject: $ProductMetadata, $PropertyToIndex: "PrintImageTemplateNames"})
+    }
+    return $PrintImageTemplateNameToSizeAndFormTypeIndex
+}
+
+var $ColorCodeToMarketingNameIndex
+export async function Get_TervisProductColorCodeToMarketingNameIndex () {
+    if (!$ColorCodeToMarketingNameIndex) {
+        var $ColorCodeToMarketingNameMapping = await Get_TervisProductColorCodeToMarketingNameMapping()
+        $ColorCodeToMarketingNameIndex = New_HashTableIndex({ $InputObject: $ColorCodeToMarketingNameMapping, $PropertyToIndex: "ColorCode"})
+    }
+    return $ColorCodeToMarketingNameIndex
+}
+
+export function Get_TervisProductSizeAndFormTypeFromString ({
+    $String
+  }) {
+    var $Results = ConvertFrom_StringUsingRegexCaptureGroup({
+        $Regex: /(?<$Size>\d*)(?<$FormType>\w*)/u,
+        $String
+    })
+
+    if ($Results) {
+        return $Results
+    }
 }
