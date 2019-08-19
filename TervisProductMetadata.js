a729968df5e824c360951db614c5d3913aaa7ed8
Chris Magnuson <hourglasssand@gmail.com>
2019-08-19 17:19:31 -0400

    Update json version for color codes

--- TervisProductMetadata.js
+++ TervisProductMetadata.js
@@ -132,7 +132,7 @@ export async function Get_TervisProductColorCodeToMarketingNameMapping() {
         var fetch = window.fetch
     }
 
-    var versionOfColorCodeToMarketingNameMapping = "1.0.3";
+    var versionOfColorCodeToMarketingNameMapping = "1.0.14";
 
     return await fetch(`https://unpkg.com/@tervis/tervisproductmetadata@${versionOfColorCodeToMarketingNameMapping}/ColorCodeToMarketingNameMapping.json`)
     .then(response => response.json())
