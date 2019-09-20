edcc49a4c705fac84153075e76cbd600e17dcde2
ChrisMagnuson <hourglasssand@gmail.com>
2019-09-20 15:17:09 -0400

    Bump json version

--- TervisProductMetadata.js
+++ TervisProductMetadata.js
@@ -14,7 +14,7 @@ export async function Get_TervisProductMetaData() {
         var fetch = window.fetch
     }
 
-    var $VersionOfProductMetaData = "1.0.19";
+    var $VersionOfProductMetaData = "1.0.20";
 
     var $ProductMetadata = await fetch(
         `https://unpkg.com/@tervis/tervisproductmetadata@${$VersionOfProductMetaData}/TervisProductMetadata.json`
