38b278b09645fb34314c088d5cf236231c2d4d27
Hamilton Perez <hambone124@users.noreply.github.com>
2020-03-25 10:31:45 -0400

    Add data on 24WMB

--- TervisProductMetadata.js
+++ TervisProductMetadata.js
@@ -14,7 +14,7 @@ export async function Get_TervisProductMetaData() {
         var fetch = window.fetch
     }
 
-    var $VersionOfProductMetaData = "1.0.33";
+    var $VersionOfProductMetaData = "1.0.35";
 
     var $ProductMetadata = await fetch(
         `https://unpkg.com/@tervis/tervisproductmetadata@${$VersionOfProductMetaData}/TervisProductMetadata.json`
