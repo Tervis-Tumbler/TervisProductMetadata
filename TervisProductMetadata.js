d4e68f15f5fe98178d81fe3e8165437716b8f0d1
Hamilton Perez <hambone124@users.noreply.github.com>
2022-09-20 17:06:27 -0400

    Adjust rotation values for 20/30VIT

--- TervisProductMetadata.js
+++ TervisProductMetadata.js
@@ -14,7 +14,7 @@ export async function Get_TervisProductMetaData() {
         var fetch = window.fetch
     }
 
-    var $VersionOfProductMetaData = "1.0.51";
+    var $VersionOfProductMetaData = "1.0.52";
 
     var $ProductMetadata = await fetch(
         `https://unpkg.com/@tervis/tervisproductmetadata@${$VersionOfProductMetaData}/TervisProductMetadata.json`
